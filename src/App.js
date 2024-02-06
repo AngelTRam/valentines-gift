1c055690af71637298662788ee09f49306d6bdbd
Angel Tapia <angel.t.r42@gmail.com>
2024-02-06 13:46:03 -0700

    Home added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useNavigate } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
+  function handleClick(){
+    navigate("./pages/letter.js")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>FELIZ SAN VALENTIN</p>
+        <img src={require("./img/my-melody-love.gif")} className="Melody" onClick={handleClick}/>
+        <h2>Un lugar hecho con mucho amor para ti, mi vida.</h2>
       </header>
     </div>
   );
---
+++ src/pages/letter.js
@@ -0,0 +1,11 @@
+import './style/letter.css';
+
+function Letter() {
+  return (
+    <div className="Letter">
+      HOLA
+    </div>
+  );
+}
+
+export default Letter;
